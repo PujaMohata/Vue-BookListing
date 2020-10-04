b5d08adcfbb3e70b1c25e1c77fb0db5bae91749e
Puja <pujamohata96@gmail.com>
2020-10-04 18:01:29 +0530

    3rd work

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
-<template>
+<template v-for "book in books">
 <div>
 <h1>{{title}}</h1>
+<ul>
+<li>{{book.title}} : {{book.author}}</li>
+</ul>
 </div>
 </template>
 
@@ -9,8 +12,12 @@
 export default{
 name: 'BookList',
 data(){
-return {
-title : 'All Books'
+return{
+books:[
+ {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+    {title: 'American Gods', author: 'Neil Gaiman'},
+    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+]
 }
 }
 }
@@ -19,4 +26,6 @@ title : 'All Books'
 
 <style>
 h1,h2 {font-weight:normal}
+ul {list-style-type:none, padding:0}
+li {display:block,margin:0 10px}
 </style>
